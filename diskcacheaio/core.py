b38acf734915f7b872deda72f38b6aa30e98ae64
Soumyadip Das Mahapatra <soumyadip.bt@gmail.com>
2018-05-29 22:14:51 +0530

    Initial commit

---
+++ diskcacheaio/core.py
@@ -0,0 +1,14 @@
+"""Core component of the library"""
+
+import aioodbc
+import asyncio
+
+
+class CachedDictionary:
+    """A SQLite based dictionary"""
+    def __init__(self, db_path: str):
+        """
+        :param db_path: path to the database file
+        """
+        self._dsn = "Driver=SQLite;Database={}".format(db_path)
+        self._connection = aioodbc.connect(dsn=self._dsn, loop=asyncio.get_event_loop())
