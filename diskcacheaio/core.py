08971ea01cbdb26870eb46ad5cea4922ba488c64
Soumyadip Das Mahapatra <soumyadip.bt@gmail.com>
2018-05-29 22:38:48 +0530

    autopep8

--- diskcacheaio/core.py
+++ diskcacheaio/core.py
@@ -1,14 +1,17 @@
 """Core component of the library"""
 
-import aioodbc
 import asyncio
+
+import aioodbc
 
 
 class CachedDictionary:
     """A SQLite based dictionary"""
+
     def __init__(self, db_path: str):
         """
         :param db_path: path to the database file
         """
         self._dsn = "Driver=SQLite;Database={}".format(db_path)
-        self._connection = aioodbc.connect(dsn=self._dsn, loop=asyncio.get_event_loop())
+        self._connection = aioodbc.connect(
+            dsn=self._dsn, loop=asyncio.get_event_loop())
